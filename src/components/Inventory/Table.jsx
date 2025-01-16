d1d513a51ad5bfdbe1f24a0cfc827e7f7060b265
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2025-01-16 22:21:57 +0530

    product

--- src/components/Inventory/Table.jsx
+++ src/components/Inventory/Table.jsx
@@ -84,12 +84,6 @@ const InventoryTable = ({
                         type="text"
                         className="border  bg-green-400 p-2"
                         value={editingProduct.stock}
-                        onChange={(e) =>
-                          setEditingProduct({
-                            ...editingProduct,
-                            stock: e.target.value,
-                          })
-                        }
                       />
                     ) : (
                       product.stock
