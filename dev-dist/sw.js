642a7132123fc707e30fba62011a4cd8a19715e1
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2025-01-21 21:38:15 +0530

    dev

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jnph9uf2abo"
+    "revision": "0.5sp6e7eru1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
